34320f54cfbe3b156a2464d884ed89e529c27840
Adhocmaster <adhocmaster@live.com>
2019-01-15 00:13:09 -0800

    user module added

--- app/src/main/java/com/adhocmaster/roundtablediscussion/framework/StructuredAppCompatActivity.kt
+++ app/src/main/java/com/adhocmaster/roundtablediscussion/framework/StructuredAppCompatActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.adhocmaster.roundtablediscussion.R
 
-abstract class StructuredAppCompatActivity: AppCompatActivity(), StructuredActivity {
+abstract class StructuredAppCompatActivity:
+        AppCompatActivity(),
+        StructuredActivity {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
---
+++ app/src/main/java/com/adhocmaster/roundtablediscussion/user/User.kt
@@ -0,0 +1,8 @@
+package com.adhocmaster.roundtablediscussion.user
+
+data class User(
+        val id: Int,
+        val name: String,
+        val email: String,
+        val accessToken: String
+)
